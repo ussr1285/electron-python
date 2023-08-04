f65595827dcfcda39c0ac12d75f2ab526fff2632
Minjae Joe <ussr1285@ajou.ac.kr>
2023-08-04 17:36:12 +0900

    electron으로 직접 터미널로 python 실행 불가능한 것으로 판정.
    electron도 웹 기반이라, 불가능한 것으로 판단.

--- functions.js
+++ functions.js
@@ -1,25 +1,35 @@
-// const os = 
 const common_info = ["appName", "appSite", "siteDomain", "appId", "appVersion", "onesignalPushId"]
 const android_info = ["appVersionCode", "androidKeystorePath", "androidKeystorePassword", "androidAlias", "androidAliasPassword"]
 
 const getAppInfo = () => {
     let info = {}
+    let os_radio = document.getElementsByName('os_type');
+    let os_type
+
+    for(var i=0; i < os_radio.length; i++) {
+        if(os_radio[i].checked === true) {
+            os_type = os_radio[i].value;
+      }
+    }
+    
     common_info.forEach(inputInfo => {
         info = {...info, [inputInfo]:document.getElementById(inputInfo).value}
-        // info.push(document.getElementById(inputInfo).value)
     });
-    android_info.forEach(inputInfo => {
-        info = {...info, [inputInfo]:document.getElementById(inputInfo).value}
-        // info.push(document.getElementById(inputInfo).value)
-    });
-
+    if(os_type == "android"){
+        android_info.forEach(inputInfo => {
+            info = {...info, [inputInfo]:document.getElementById(inputInfo).value}
+        });
+    }
+    info = {...info, ["os_type"]:os_type}
     alert(JSON.stringify(info))
 }
 
-
 document.getElementById('btnEd').addEventListener('click', () => {
+    // Open a local file in the default app
+    // alert("hello")
     getAppInfo()
 })
+
 
 // let os = document.getElementById("os").value
 /*
--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+'use strict';
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
+        nodeIntegration: true,
         contextIsolation: true,
         preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   ipcMain.handle("set-name", async function setGoal(_event, value){
-    console.log(value)
+    console.log(value);
   })
 
 }
 
 app.whenReady().then(async () => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
 })
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
+
+//////////////////////////////////////////////////////////////////////////////////
